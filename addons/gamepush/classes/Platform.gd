2c53f6ae6c74c9160bc2d5157ea79965faabd63e
Kulagin Anatoliy <talkafk@gmail.com>
2024-09-25 12:16:26 +0200

    platform

--- addons/gamepush/classes/Platform.gd
+++ addons/gamepush/classes/Platform.gd
@@ -1,11 +1,54 @@
 extends Node
 
+var window:JavaScriptObject
+var gp:JavaScriptObject
+var platform:JavaScriptObject
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if OS.get_name() == "Web":
+		window = JavaScriptBridge.get_interface("window")
+		while not gp:
+			gp = window.gp
+			await get_tree().create_timer(0.1).timeout
+		platform = gp.platform
 
+# Проверка типа платформы, например Yandex или Vk
+func type():
+	if OS.get_name() == "Web":
+		return platform.type
+	push_warning("Not Web")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# Проверка, доступна ли интегрированная аутентификация
+func has_integrated_auth():
+	if OS.get_name() == "Web":
+		return platform.hasIntegratedAuth
+	push_warning("Not Web")
+
+# Проверка, можно ли выйти из системы
+func is_logout_available():
+	if OS.get_name() == "Web":
+		return platform.isLogoutAvailable
+	push_warning("Not Web")
+
+# Проверка, разрешено ли открытие внешних ссылок
+func is_external_links_allowed():
+	if OS.get_name() == "Web":
+		return platform.isExternalLinksAllowed
+	push_warning("Not Web")
+
+# Проверка, доступна ли аутентификация через секретный код
+func is_secret_code_auth_available():
+	if OS.get_name() == "Web":
+		return platform.isSecretCodeAuthAvailable
+	push_warning("Not Web")
+
+func get_SDK():
+	if OS.get_name() == "Web":
+		return await platform.getSDK()
+	push_warning("Not Web")
+	
+func get_native_SDK():
+	if OS.get_name() == "Web":
+		return await platform.getNativeSDK()
+	push_warning("Not Web")
