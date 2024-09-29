b07dc357f4aaba3bc4149b65126f3c01375d4c03
Kulagin Anatoliy <talkafk@gmail.com>
2024-09-29 11:34:57 +0200

    Segments

--- addons/gamepush/classes/Segments.gd
+++ addons/gamepush/classes/Segments.gd
@@ -1,11 +1,37 @@
 extends Node
 
+var window:JavaScriptObject
+var gp:JavaScriptObject
+var segments:JavaScriptObject
 
-# Called when the node enters the scene tree for the first time.
+signal entered(segment_tag: String)
+signal left(segment_tag: String)
+
 func _ready():
-	pass # Replace with function body.
+	if OS.get_name() == "Web":
+		window = JavaScriptBridge.get_interface("window")
+		while not gp:
+			gp = window.gp
+			await get_tree().create_timer(0.1).timeout
+		segments = gp.segments
+		gp.segments.on("enter", JavaScriptBridge.create_callback(func(segment_tag):
+			entered.emit(segment_tag)
+		))
+		gp.segments.on("leave", JavaScriptBridge.create_callback(func(segment_tag):
+			left.emit(segment_tag)
+		))
 
+func list() -> Array:
+	if OS.get_name() == "Web":
+		var segment_list: Array = []
+		segments.list.forEach(JavaScriptBridge.create_callback(func(segment: String): segment_list.append(segment)))
+		return segment_list
+	push_warning("Not running on Web")
+	return []
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func has(tag: String) -> bool:
+	if OS.get_name() == "Web":
+		var has_payments = await segments.has(tag)
+		return has_payments
+	push_warning("Not running on Web")
+	return false
